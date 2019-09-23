445e67b06ed5ec2360dba484c057c6b2fada0265
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-23 20:06:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.max_by{|key, value| value}
+  name_hash.min_by{|key, value| value}
 end
